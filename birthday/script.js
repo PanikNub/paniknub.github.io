78d830725aa16c6b8ec0e2f1bf8c5c894029564f
PanikNub <alexander.nguyen14643@gmail.com>
2024-10-19 11:41:44 -0500

    Add files via upload

--- birthday/script.js
+++ birthday/script.js
@@ -2,14 +2,8 @@
 
 let birthdaySong = document.getElementById("birthdaySong");
 let confettiButton = document.getElementById("confettiButton");
-let pikaImage = document.getElementById("pikaImage");
 
 confettiButton.onclick = function(){
     let audio = document.getElementById("confettiAudio");
-    audio.play();
-}
-pikaImage.onclick = function(){
-    let audio = document.getElementById("pikaSound");
-    audio.volume = 0.5;
     audio.play();
 }
--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+let switchModes = document.getElementById("switchModes");
+switchModes.onclick = function(){
+    document.body.classList.toggle("switchModes");
+}
